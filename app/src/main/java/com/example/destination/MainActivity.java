5a6d548bc6fc8feda521a5afa7034cedd1edfc7c
Paras Gautam <48523128+ParasGautam26@users.noreply.github.com>
2020-11-21 01:10:06 +0530

    first commit

---
+++ app/src/main/java/com/example/destination/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.destination;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView mRecyclerView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
